063ba583130409010bf4ef60def65852be524918
Alex Boten <aboten@lightstep.com>
2023-11-07 09:58:44 -0600

    wip
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- index.js
+++ index.js
@@ -1,9 +1,71 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
+const {
+  getNodeAutoInstrumentations,
+} = require('@opentelemetry/auto-instrumentations-node');
+const {
+  PeriodicExportingMetricReader,
+  ConsoleMetricExporter,
+} = require('@opentelemetry/sdk-metrics');
+const opentelemetry = require('@opentelemetry/api');
 
-try {
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
+const sdk = new NodeSDK({
+    traceExporter: new ConsoleSpanExporter(),
+    metricReader: new PeriodicExportingMetricReader({
+      exporter: new ConsoleMetricExporter(),
+    }),
+    instrumentations: [getNodeAutoInstrumentations()],
+  });
+  
+sdk.start();
+
+async function run() {
+    
+    //...
+
+    const tracer = opentelemetry.trace.getTracer(
+    'instrumentation-scope-name',
+    'instrumentation-scope-version',
+    );
+    const span = tracer.startSpan('op');
+    span.setAttribute('key', 'value');
+    span.end();
+
+    // This should be a token with access to your repository scoped in as a secret.
+    // The YML workflow will need to set myToken with the GitHub Secret Token
+    // myToken: ${{ secrets.GITHUB_TOKEN }}
+    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+    const myToken = core.getInput('repo-token');
+    const endpoint = core.getInput('endpoint');
+    const headers = parseHeaders(core.getInput('headers'))
+    const repository = github.context.repo
+    const runID = github.context.runNumber
+    const octokit = github.getOctokit(myToken)
+
+
+    // You can also pass in additional options as a second parameter to getOctokit
+    // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
+
+    // const { data: pullRequest } = await octokit.rest.pulls.get({
+    //     owner: 'octokit',
+    //     repo: 'rest.js',
+    //     pull_number: 123,
+    //     mediaType: {
+    //       format: 'diff'
+    //     }
+    // });
+
+    // console.log(pullRequest);
 }
+
+run();
+
+
+// try {
+//     const payload = JSON.stringify(github.context.payload, undefined, 2)
+//     console.log(`The event payload: ${payload}`);
+// } catch (error) {
+//     core.setFailed(error.message);
+// }
